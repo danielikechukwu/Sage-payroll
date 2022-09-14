d59523edc4a4fcdbe1e70eb637a146345e487db9
Daniel ikechukwu <danielikechukwu495@gmail.com>
2022-09-14 15:01:59 +0100

    updated version of Sage-payroll-webapp

--- src/components/section/Navigator/Accordion.js
+++ src/components/section/Navigator/Accordion.js
@@ -1,48 +1,76 @@
-import * as React from 'react';
+import React, {useState, useRef} from 'react';
 import styled from 'styled-components';
+import './Accordion.css';
 
 
-function Navigator() {
+function Navigator(source) {
+
+  const [setActive, setActiveState] = useState("");
+
+  const [setHeight, setHeightState] = useState("0px");
+
+  const content = useRef(null)
+
+  const ToggleAccordionHandler = () =>{
+
+    setActiveState( setActive === "" ? "active" : "")
+    setHeightState( setActive ==="" ? "0px" : `${content.current.scrollHeight}px`)
+
+  }
   return (
 
-    <Accordionwrapper>
+    
 
       <Accordionsection>
 
       <Button>
-        <ButtonText></ButtonText>
+
+        <button onClick={ToggleAccordionHandler}>{source.title}</button>
+
       </Button>
 
-      <Accordioncontent>
-        <li>Hello</li>
-        <li>Hello</li>
-        <li>Hello</li>
-        <li>Hello</li>
+      <Accordioncontent ref={content} style={{ maxHeight: `${setHeight}`}}>
+
+        {source.content}     
+
       </Accordioncontent>
 
       </Accordionsection>
 
-    </Accordionwrapper>
+   
 
   );
 }
 
 export default Navigator
 
-const Accordionwrapper = styled.div`
-/* 
-    background-color: #fff;
-    width: 20%;
-    padding-top: 4%;
-    padding-left: 1%;
-    border-right: 1px solid #dee2e6; */
+
+
+const Accordioncontent = styled.div`
+
+margin-left: 10%;
+font-family: 'Didact Gothic', sans-serif;
+font-size: 14px;
+overflow: hidden;
+transition: max-height 0.6s ease;
+/* max-height: 100px; */
 
 `;
 
-const Accordioncontent = styled.div``;
-
 const Accordionsection = styled.div``;
 
-const Button = styled.div``;
+const Button = styled.div`
 
-const ButtonText = styled.div``;
+button {
+  /* border: 1px solid red; */
+  border: none; 
+  width: 100%;
+  padding-top: 5%;
+  padding-bottom: 4.3%;
+  text-align: left;
+  background-color: transparent;
+  font-family: 'Didact Gothic', sans-serif;
+    ;
+
+}
+`;
--- src/components/section/Navigator/Navigator.js
+++ src/components/section/Navigator/Navigator.js
@@ -1,20 +1,60 @@
 import * as React from 'react';
 import Accordion  from './Accordion';
-import './Navigator.css';
+// import './Navigator.css';
+import styled from 'styled-components';
 
 function Navigator() {
 
   return (
 
-    <div className='container'>  
+    <Wrapper> 
 
-        <Accordion />    
+    <Accordionwrapper> 
 
-    </div>
+        <Accordion title="Employees" content={<><li>Hello</li> <li>Hello</li> <li>Hello</li></>}/>  
+        <Accordion title="Company Structure" content={<><li>Hello</li> <li>Hello</li> <li>Hello</li></>}/>  
+        <Accordion title="Leaves" content={<><li>Hello</li> <li>Hello</li> <li>Hello</li></>}/>
+        <Accordion title="Subsidiaries" content={<><li>Hello</li> <li>Hello</li> <li>Hello</li></>}/>
+        <Accordion title="Policies" content={<><li>Hello</li> <li>Hello</li> <li>Hello</li></>}/>
+        <Accordion title="Payroll" content={<><li>Hello</li> <li>Hello</li> <li>Hello</li></>}/>
+        <Accordion title="Remuneration" content={<><li>Hello</li> <li>Hello</li> <li>Hello</li></>}/>
+        <Accordion title="Tax Management" content={<><li>Hello</li> <li>Hello</li> <li>Hello</li></>}/>
+        <Accordion title="Transactions" content={<><li>Hello</li> <li>Hello</li> <li>Hello</li></>}/>
+        <Accordion title="Setting" content={<><li>Hello</li> <li>Hello</li> <li>Hello</li></>}/>
+        <Accordion title="Export/Import" content={<><li>Hello</li> <li>Hello</li> <li>Hello</li></>}/>
+        <Accordion title="User Access" content={<><li>Hello</li> <li>Hello</li> <li>Hello</li></>}/> 
+
+    </Accordionwrapper>
+
+    </Wrapper>
 
   );
 
 }
 
 export default Navigator
+
+const Wrapper = styled.div`
+
+    background-color: #fff;
+    width: 20%;
+    padding-top: 2%;
+    padding-left: 1%;
+    box-shadow: 0 0  8px 2px rgba(0, 0, 0, 0.26);
+    border-right: 1px solid #dee2e6;
+    /* border: 1px solid red; */
+`;
+
+const Accordionwrapper = styled.div`
+
+    /* border: 1px solid red; */
+    overflow-y: scroll;
+    margin-right: 1%;
+    margin-bottom: 12%; 
+    height: 90%;
+    
+    &::-webkit-scrollbar {
+    display: none; /* for Chrome, Safari, and Opera */
+}
+`;
 
